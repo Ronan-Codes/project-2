7f35dc48ff1c6044446eeb33860c6ed7d320232c
Ronan <galv.ronan@gmail.com>
2021-05-27 20:58:11 -0400

    Added more starter files

---
+++ utils/auth.js
@@ -0,0 +1,14 @@
+// Example
+
+/*
+    const withAuth = (req, res, next) => {
+        if (!req.session.user_id) {
+            res.redirect('/login');
+        } else {
+            next();
+        }
+    };
+
+module.exports = withAuth;
+
+*/
