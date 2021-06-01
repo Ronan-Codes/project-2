f4302d7da18233df6c1ab1324c7b523350aef87a
Ronan <galv.ronan@gmail.com>
2021-05-31 23:55:45 -0400

    Models and server.js completed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 // const ____ = require('./____');
+const userRoutes = require('./user-routes');
+const journalentryRoutes
 
 // router.use('/____', _____);
 
---
+++ models/JournalEntry.js
@@ -0,0 +1,51 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection')
+
+class JournalEntry extends Model {}
+
+// create fields/columns for JournalEntry model
+JournalEntry.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_grateful_input: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        second_grateful_input: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        third_grateful_input: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        freewrite_input: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        mood_input: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = JournalEntry;
