acba600aac1fa6783948dba24a3052263d9dabf5
Ronan <galv.ronan@gmail.com>
2021-06-26 00:49:56 -0400

    Adjust styles. Remove New Entry button and replace with calendar's Today's Entry button

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -10,6 +10,24 @@
         return `${day} ${month} ${year}`;
     }
 
+    function formatToday() {
+        var d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+
+    const startDate = [reformatDate(formatToday())]
+
+    console.log(startDate)
+
     async function selectDayEntry(date_input) {
 
         const response = await fetch(`/api/journalentries/${date_input}`, {
@@ -17,12 +35,10 @@
         });
 
         if (!response.ok) {
-            // document.location.replace(`/dashboard/journalentry/${date_input}`);
             // insert modal function instead of alert
             alert("no journal entry for this day")
             return
         } else {
-            // document.location.replace(`/dashboard/newjournalentry`);
             document.location.replace(`/dashboard/journalentry/${date_input}`);
         }
     }
@@ -35,33 +51,33 @@
         });
 
         if(response.ok) {
-            console.log(response)
             response.json().then(function (journalEntries) {
                 if (journalEntries.length == 0) {
                     $('#datepicker').Zebra_DatePicker({
-                        // don't click future dates or unshaded ones?
+                        show_select_today: "Today's Entry",
+                        show_clear_date: false,
                         always_visible: $('.datepickerContainer'),
-                        // disabled_dates: ['25 6 2021'],
+                        disabled_dates: ['* * *'],
+                        enabled_dates: startDate,
                         onSelect: function (reg_date, date_time, js_date) {
-                            selectDayEntry(reg_date)
+                            // selectDayEntry(reg_date)
+                            document.location.replace(`/dashboard/newjournalentry`);
+
                         }
                     })
                 }
-
-                console.log(journalEntries)
 
                 // for loop or map each journalEntries
                 journalEntries.map(journalDate => {
                     const formattedDate = reformatDate(journalDate.reg_date);
                     formattedDatesArr.push(formattedDate)
-                    console.log(journalDate.reg_date)
-                    console.log(formattedDatesArr)
-                    // inside loop, reformat its date to cron's syntax
 
                     $('#datepicker').Zebra_DatePicker({
-                        // don't click future dates or unshaded ones?
+                        show_select_today: "Today's Entry",
+                        show_clear_date: false,
                         always_visible: $('.datepickerContainer'),
-                        // disabled_dates: ['25 6 2021'],
+                        disabled_dates: ['* * *'],
+                        enabled_dates: formattedDatesArr,
                         custom_classes: {
                             'myclass1':  formattedDatesArr
                         },
@@ -69,7 +85,6 @@
                             selectDayEntry(reg_date)
                         }
                     })
-                    // add this to the formattedDates array
                 })
             })
         } else {
