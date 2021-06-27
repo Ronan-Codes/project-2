b2a77e66911261c2292674a911d4f76b68d9be57
Ronan <galv.ronan@gmail.com>
2021-06-26 22:27:26 -0400

    debugg logout function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -113,6 +113,7 @@ router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
+      alert("logged out")
     });
   } else {
     res.status(404).end();
